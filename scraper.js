39417028afa5af5223e4605a543f39cfd87dad16
Mimansa Sahay <mimansa192@gmail.com>
2025-04-24 21:22:20 +0530

    Added args in scraper.js

--- scraper.js
+++ scraper.js
@@ -23,7 +23,11 @@ const CONFIG = {
 
 
 const scrapeStocksToWatch = async () => {
-  const browser = await puppeteer.launch({ headless: false }); // Run in non-headless mode for debugging
+ const browser = await puppeteer.launch({
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+ });
+    
   const page = await browser.newPage();
   await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36');
   await page.setExtraHTTPHeaders({
