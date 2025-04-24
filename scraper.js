fbd29226a40a00d42c23660ab60a8b22e40bc87f
Mimansa Sahay <mimansa192@gmail.com>
2025-04-24 22:34:59 +0530

    updates

--- scraper.js
+++ scraper.js
@@ -24,20 +24,26 @@ const CONFIG = {
 
 const scrapeStocksToWatch = async () => {
  const browser = await puppeteer.launch({
-        headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
+        headless: false,
+        args: ['--no-sandbox', '--disable-setuid-sandbox','--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-gpu',
+      '--single-process']
  });
     
   const page = await browser.newPage();
   await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36');
   await page.setExtraHTTPHeaders({
       'accept-language': 'en-US,en;q=0.9',
+      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
   });
 
   try {
-      await page.goto('https://www.moneycontrol.com/news/tags/stocks-to-watch.html', { waitUntil: 'networkidle2', timeout: 120000 });
-      await page.waitForSelector('#newslist-0 h2 a', { timeout: 10000 }); 
-  
+      await page.goto('https://www.moneycontrol.com/news/tags/stocks-to-watch.html', { waitUntil: 'networkidle2', timeout: 180000 });
+      console.log('Waiting for selector...');
+      await page.waitForSelector('#newslist-0 h2 a', { timeout: 30000 }); 
+    
+
       const articleUrl = await page.evaluate(() => {
           const firstAnchor = document.querySelector('#newslist-0 h2 a');
           return firstAnchor ? firstAnchor.href : null;
@@ -48,7 +54,8 @@ const scrapeStocksToWatch = async () => {
       if (articleUrl) {
           const articlePage = await browser.newPage();
           await articlePage.goto(articleUrl);
-         
+          await articlePage.waitForSelector('p', { timeout: 30000 });
+
           const data = await articlePage.evaluate(() => {
               const allP = Array.from(document.querySelectorAll('p'));
               const result = [];
